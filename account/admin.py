282a21d35063ecec7d4de28fb49201df5a93f3df
user <triplek901@gmail.com>
2021-04-13 12:59:38 +0300

    add

--- account/admin.py
+++ account/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from account.models import Users, Neighborhood
 
 # Register your models here.
+
 class AccountAdmin(UserAdmin):
     list_display = ('name','email', 'username','date_joined', 'last_login','is_admin','is_neighborhood_admin','neighborhood_name')
     search_fields = ('email', 'username','neighborhood_name')
